1f2f51a08e93736d018d3fda4e237414560eeb9e
jstgrowup <deysubham999@gmail.com>
2023-11-24 15:15:01 +0530

    done

--- revision/chapter1/index.js
+++ revision/chapter1/index.js
@@ -3,36 +3,47 @@ import ReactDOM from "react-dom/client";
 // hot module reload
 // file watchers
 //
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "heading 1 for parcel"
-);
-console.log("heading:", heading);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "Heading 2 for my use"
-);
-const headingUsingJSX = (
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "title",
+//     key: "h1",
+//   },
+//   "heading 1 for parcel"
+// );
+// console.log("heading:", heading);
+// const heading2 = React.createElement(
+//   "h2",
+//   {
+//     id: "title",
+//     key: "h2",
+//   },
+//   "Heading 2 for my use"
+// );
+const HeadingUsingJSX = () => (
   <h1 tabIndex="1" className="header">
-    Namaste react
+    Heading
   </h1>
 );
 // this is JSX
-const box = React.createElement(
-  "div",
-  {
-    id: "sample div",
-    key: "div",
-  },
-  [heading, headingUsingJSX]
-);
+// const box = React.createElement(
+//   "div",
+//   {
+//     id: "sample div",
+//     key: "div",
+//   },
+//   [headingUsingJSX]
+// );
+
+const Head = () => {
+  return (
+    <div>
+      {/* {HeadingUsingJSX()} */}
+      <HeadingUsingJSX />
+      <h1>Namaste react</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(box);
+root.render(<Head />);
